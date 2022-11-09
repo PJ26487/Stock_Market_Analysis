4b32e52f41c0b7a9008024a313521cf1eb287be1
Prajjwal Arya <90100095+PJ26487@users.noreply.github.com>
2022-11-09 11:39:40 +0530

    Update yfinance.py

--- yfinance.py
+++ yfinance.py
@@ -7,7 +7,7 @@ import time
 import spacy 
 from spacy import displacy
 from collections import Counter
-spacy.download(en_core_web_md)
+md = spacy.load(en_core_web_md)
 import en_core_web_md
 
 import json 
